f3a39f9f95f89154749ba447e52f639f01f3778f
metalx1000 <github@filmsbykris.com>
2015-06-17 14:26:44 -0400

    Tweeked enmey speed and timing

--- js/enemy.js
+++ js/enemy.js
@@ -1,4 +1,7 @@
 var enemy_timer;
+//set original delay for new enemy spawning
+var enemy_wait1 = 3000;
+var enemy_wait2 = 1000;
 
 function preload_enemy(){
   game.load.image('enemy', 'res/sprites/alien_ship.png');
@@ -17,25 +20,35 @@ function update_enemy(){
 
   enemies.forEach(function(enemy){
     if(game.time.now > enemy.shootTime && enemy.alive == true){
-      shoot(enemy,5,"enemy-bullet");
-      enemy.shootTime = game.time.now + Math.floor(Math.random() * 5000) + 1000;
+      shoot(enemy,10,"enemy-bullet");
+      enemy.shootTime = game.time.now + Math.floor(Math.random() * enemy_wait1) + 1000;
     }
   });
 }
 
 function new_enemy(){
-  //set enemy speed between 5,000 and 10,000
-  var speed = Math.floor(Math.random() * 10000) + 5000;
   var enemy = enemies.create(game.world.randomX,-64,'enemy');
   enemy.scale.setTo(.5,.5);
   enemy.anchor.setTo(.5,.5);
-  game.add.tween(enemy).to({y: game.world.height+256}, speed, "Linear", true);
+
+  //set enemy speed between 2,000 and 5,000
+  //lower values makes the ship faster
+  enemy.speed = Math.floor(Math.random() * 5000) + 2000;
+  game.add.tween(enemy).to({y: game.world.height+256}, enemy.speed, "Linear", true);
   enemy.ship = true;
 
   //set random shoot time for enemy
-  enemy.shootTime = game.time.now + Math.floor(Math.random() * 5000) + 1000;
+  enemy.shootTime = game.time.now + Math.floor(Math.random() * 3000) + 1000;
   //set delay for next enemy between 1 and 3 seconds
-  var delay = Math.floor(Math.random() * 3000) + 1000
+  //And speed up as time goes on
+  var delay = Math.floor(Math.random() * enemy_wait1) + enemy_wait2
+  if(enemy_wait1 > 2000){
+    enemy_wait1-=10
+  }
+  if(enemy_wait2 > 500){
+    enemy_wait2-=10
+  }
+
   enemy_timer = game.time.now + delay
 
 }
